a4ceb10f790a6d906af245cf654de0c29757cc0a
Rayane TROUDI <rayanetroudi1@gmail.com>
2022-12-20 22:25:06 +0100

    V1.0 de TraitementImage.java

---
+++ TraitementImage.java
@@ -0,0 +1,36 @@
+/** ----- PACKAGES LECTURE IMAGE PNG ----- **/
+import projets5.*;
+
+
+/** ----- PACKAGES TRAITEMENTS FICHIERS ----- **/
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.Files;
+
+
+/** ----- PACKAGES STRUCTURES DE DONNEES ------ **/
+
+
+
+public abstract class TraitementImage {
+    
+    /** ----- ATTRIBUTS DE CLASSE ------ **/
+
+    private Path path_img;
+
+
+    /** ----- CONSTRUCTEUR ----- **/
+    public TraitementImage(String path_img){
+
+        this.path_img = Paths.get(path_img);
+
+        if (!Files.exists(this.path_img)) {
+
+            this.path_img = null;
+        }
+
+    }
+
+}

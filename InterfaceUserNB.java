4740a52a115bee660e625474d94fef321d5184f1
Kaporals610 <florent.gerbaud.jamin@gmail.com>
2022-12-24 10:10:19 +0100

    interface utilisateur. Reste le commentaire que je vais faire

--- InterfaceUserNB.java
+++ InterfaceUserNB.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 
-public class InterfaceUserNB extends TraitementImageNoirBlanc{
+public class InterfaceUserNB{
     
     private String traitement;
     private String path_img;
+    private String imgRename;
+    private String plusieurTraitement;
+    private String stop;
 
-    public InterfaceUserNB(String path_img){
-        super(path_img);
+    public InterfaceUserNB(){
+    
         this.traitement="";
         this.path_img="";
+        this.imgRename="tamere";
+        this.plusieurTraitement="";
+        this.stop="";
     }
 
     public void traitementUser(){
@@ -28,37 +34,74 @@ public class InterfaceUserNB extends TraitementImageNoirBlanc{
         System.out.println("Vous avez choisis : " + this.path_img);  // Output user input
     }
 
+    public boolean plusieurTraitement(){
+
+        Scanner image = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("aurez vous plusieur traitement à réaliser ? ");
+        this.plusieurTraitement = image.nextLine();  // Read user input
+        if(this.plusieurTraitement.equals("oui")){
+            return true;
+        }
+        return false;
+    }
+
+    public String stop(){
+
+        Scanner image = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("si vous avez fini tapez stop : ");
+        this.stop = image.nextLine();  // Read user input
+        return this.stop;
+    }
+
+    public void renameImg(){
+
+        String[] cheminAcces = this.path_img.split("/");
+        String nameImg=cheminAcces[cheminAcces.length-1];
+        System.out.println("test : "+ cheminAcces[cheminAcces.length-1]);
+        String [] accsName=nameImg.split(".png");
+        System.out.println(accsName.length);
+        this.imgRename=accsName[0]+this.traitement+".png";
+        System.out.println(this.imgRename);
+    }
+
     public void GestionTraitement(){
 
         imageUser();
         traitementUser();
+        renameImg();
         TraitementImageNoirBlanc imageToProcess = new TraitementImageNoirBlanc(this.path_img);
         byte t[] = imageToProcess.RecupImageNoirBlanc(this.path_img);
 
         switch (this.traitement) {
             case "A":
+            System.out.println("Ta mere je suis la");
                 int img[][] = imageToProcess.imageNoirBlanc(this.path_img); // transforme notre tableau de byte en une matrice d'entier de 0 à 255
                 int [][] imgAssombri1=imageToProcess.assombrissement(img); // applique la procdeure d'assombrissement
                 byte [] nImg=imageToProcess.imageModifie(imgAssombri1); // écrit l'image assombris dans un tableau de byte
-                imageToProcess.saveImage(nImg, "MaGeuleAssombrie"); // puis sauvegarde l'image
+                imageToProcess.saveImage(nImg, this.imgRename); // puis sauvegarde l'image
+                break;
 
             case "E": //applique un traitement d'éclairage sur l'image de base
                 int img2[][] = imageToProcess.imageNoirBlanc(this.path_img);
                 int [][] imgRestaurer=imageToProcess.eclairage(img2); 
                 byte [] nImg2=imageToProcess.imageModifie(imgRestaurer);
-                imageToProcess.saveImage(nImg2, "MaGeuleEclairer");
+                imageToProcess.saveImage(nImg2, this.imgRename);
+                break;
             
             case "C": //applique un traitement de contraste sur l'image
                 int img3[][] = imageToProcess.imageNoirBlanc(this.path_img);
                 int [][] imgContraster=imageToProcess.contraste(img3); 
                 byte [] nImg3=imageToProcess.imageModifie(imgContraster);
-                imageToProcess.saveImage(nImg3, "MaGeuleContraster");
+                imageToProcess.saveImage(nImg3, this.imgRename);
+                break;
             
             case "AE":
                 //Mettre le code pour lancer l'analyse de l'exposition
+                break;
             
-            case "rgbToNb"
+            case "rgbToNb":
                 //Mettre le code pour convertir une img couleur vers noir et blnc
+                break;
             default:
                 break;
         }
--- TraitementImageNB_C.java
+++ TraitementImageNB_C.java
@@ -1,7 +1,21 @@
 public class TraitementImageNB_C {
     public static void main(String[] args){
 
-        InterfaceUserNB imageToProcess = new InterfaceUserNB("");
-        imageToProcess.GestionTraitement();
+        InterfaceUserNB imageToProcess = new InterfaceUserNB();
+        boolean pt=imageToProcess.plusieurTraitement();
+        String stop;
+
+        if(pt==true){
+
+            stop=imageToProcess.stop();
+            while (!stop.equals("stop")){
+                imageToProcess.GestionTraitement();
+                stop=imageToProcess.stop();
+            }
+        }
+        else{
+
+            imageToProcess.GestionTraitement();
+        }
     }
 }
